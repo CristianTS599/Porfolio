da7807ed0c70cba994e575d7948578d2bf45c7f6
Cristian Torres Salazar <tor8@pdx.edu>
2023-03-14 21:42:37 -0700

    adds comments and readme content

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,12 +2,18 @@ $.ajax({
   url: '/views/shared/navbar.html',
   method: 'GET',
   success: function (rslt) {
+    // inject navbar html
     document.getElementById('navbar').innerHTML = rslt;
+
+    // shared navbar ended up not working need to update
+    // about link in contact page
     let about = document.getElementById('about');
     about.href = '/views/index.html#About';
 
+    // need to remove self reference, not needed here
     let contact = document.getElementById('contact').remove();
 
+    // need to update portfolio link
     let portfolio = document.getElementById('portfolio');
     portfolio.href = '/views/index.html#cards';
   },
