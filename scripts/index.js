2647178069ec3cadc2bf68a2ffa9373dbda749f9
Cristian Torres Salazar <tor8@pdx.edu>
2023-03-14 22:30:32 -0700

    changed navbar routing in index script

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 $.ajax({
-  url: '/views/shared/navbar.html',
+  url: 'views/shared/navbar.html',
   method: 'GET',
   success: function (rslt) {
     document.getElementById('navbar').innerHTML = rslt;
